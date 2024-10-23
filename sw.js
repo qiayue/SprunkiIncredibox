0900ac2f21db8eb4310f98fde463c5fa49563a30
mojun <931554665@qq.com>
2024-10-23 18:32:23 +0800

    修改为pwa应用

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'sprunki-incredibox-v1';
+const urlsToCache = [
+  '/',
+  '/play/',
+  '/css/styles.css',
+  '/js/main.js',
+  '/img/logo.png'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
